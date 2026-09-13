84afa607fc2bd208892f8c1083a3fd6b1f362671
Embersinthewind <yiyu4755@gmail.com>
2026-09-13 15:11:20 +0800

    添加商铺缓存

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,14 +1,24 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
+import com.hmdp.entity.User;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +27,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
 
+    @Resource
+    private ShopMapper shopMapper;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryById(Long id) {
+        String shopKey = CACHE_SHOP_KEY + id;
+        //1.从redis查询商铺缓存
+        String shopJson = stringRedisTemplate.opsForValue().get(shopKey);
+        //2.判断缓存是否命中
+        if (StrUtil.isNotBlank(shopJson)) {
+            //2.1 缓存命中，直接返回商铺信息(Json转Java对象)
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+        //2.2 缓存未命中
+        //3.从数据库中查商铺
+        Shop shop = shopMapper.selectOne(new LambdaQueryWrapper<Shop>().eq(Shop::getId, id));
+        //4.判断商铺是否存在
+        if (shop == null) {
+            //4.1商铺不存在，返回404
+            return Result.fail("商铺不存在");
+        }
+        //4.2商铺存在，写入redis
+        stringRedisTemplate.opsForValue().set(shopKey, JSONUtil.toJsonStr(shop));
+
+        //返回商铺信息
+        return Result.ok(shop);
+    }
 }
